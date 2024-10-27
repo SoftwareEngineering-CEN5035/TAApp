8953a7befab9b669c5a590dcf4ae8829c7216676
Bebo561 <soto.carlodavid@gmail.com>
2024-10-27 16:24:05 -0400

    Created server folder setup

--- server/main.go
+++ server/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/base64"
+	"log"
+	"net/http"
+	"os"
+
+	"firebase.google.com/go"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"google.golang.org/api/option"
+)
 
 func main() {
-	fmt.Println("Hello World")
+    if err := godotenv.Load(); err != nil {
+        log.Fatalf("Error loading .env file: %v", err)
+    }
+
+    firebaseKeyBase64 := os.Getenv("FIREBASE_SERVICE_ACCOUNT")
+    firebaseKey, err := base64.StdEncoding.DecodeString(firebaseKeyBase64)
+    if err != nil {
+        log.Fatalf("Failed to decode service account key: %v", err)
+    }
+
+    ctx := context.Background()
+    app, err := firebase.NewApp(ctx, nil, option.WithCredentialsJSON(firebaseKey))
+    if err != nil {
+        log.Fatalf("error initializing app: %v", err)
+    }
+	
+	client, err := app.Firestore(ctx)
+    if err != nil {
+        log.Fatalf("error initializing Firestore client: %v", err)
+    }
+    defer client.Close()
+
+    e := echo.New()
+    e.GET("/", func(c echo.Context) error {
+        return c.String(http.StatusOK, "Firebase and Echo are working!")
+    })
+
+    e.Start(":9000")
 }
