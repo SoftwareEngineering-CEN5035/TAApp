ca462a58ac4965de3bbddbacf9dbe3cfeb6b7669
Bebo561 <soto.carlodavid@gmail.com>
2024-10-30 00:07:00 -0400

    Made AuthUser function

--- server/handlers/department/departmentHandler.go
+++ server/handlers/department/departmentHandler.go
@@ -10,6 +10,24 @@ import (
     "firebase.google.com/go/auth"
 )
 
+func AuthUser(ctx context.Context, tokenString string, repo *repository.Repository, authClient *auth.Client) (bool, string) {
+    token, err := authClient.VerifyIDToken(ctx, tokenString)
+    if err != nil {
+        return false, "Token invalid"
+    }
+
+    uid := token.UID
+	user, err := repo.FetchUserByUID(context.Background(), uid)
+	if err != nil {
+		return false, "User not found"
+	}
+
+    if user.Role != "Department Staff" {
+		return false, "User is not proper role"
+	}
+
+    return true, "Success"
+}
 
 func CreateCourseHandler(c echo.Context, repo *repository.Repository, authClient *auth.Client) error {
     ctx := context.Background()
@@ -20,24 +38,11 @@ func CreateCourseHandler(c echo.Context, repo *repository.Repository, authClient
         return c.JSON(http.StatusUnauthorized, map[string]string{"error": "Missing Authorization header"})
     }
     tokenString := authHeader[len("Bearer "):] 
+    isAuth, authMessage := AuthUser(ctx, tokenString, repo, authClient)
 
-    // Verify jwt token
-    token, err := authClient.VerifyIDToken(ctx, tokenString)
-    if err != nil {
-        return c.JSON(http.StatusUnauthorized, map[string]string{"error": "Invalid or expired token"})
+    if(!isAuth){
+        return c.JSON(http.StatusBadRequest, map[string]string{"error": authMessage})
     }
-
-    uid := token.UID
-
-	user, err := repo.FetchUserByUID(context.Background(), uid)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to retrieve user information"})
-	}
-
-    // Authenticate User role
-    if user.Role != "Department Staff" {
-		return c.JSON(http.StatusForbidden, map[string]string{"error": "Insufficient permissions"})
-	}
 
     // Bind json data to course object
     if err := c.Bind(&course); err != nil {
@@ -45,7 +50,7 @@ func CreateCourseHandler(c echo.Context, repo *repository.Repository, authClient
     }
 
     // Save course object
-    err = repo.CreateCourse(ctx, &course)
+    err := repo.CreateCourse(ctx, &course)
     if err != nil {
         return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
     }
