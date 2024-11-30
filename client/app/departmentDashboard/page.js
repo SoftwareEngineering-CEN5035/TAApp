89432ed2276adb223cf456de9365c21c478605cb
Bebo561 <soto.carlodavid@gmail.com>
2024-11-30 18:38:49 -0500

    Set up Dashboard

---
+++ client/app/departmentDashboard/page.js
@@ -0,0 +1,39 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import DepartmentNavbar from '../navbars/departmentNavbar';
+import Courses from './pagesDirectory/courses';
+import Applications from './pagesDirectory/applications';
+import { useRouter } from 'next/navigation';
+
+const DashboardPage = () => {
+  const router = useRouter();
+  const [selectedPage, setSelectedPage] = useState('course'); 
+
+  // useEffect(() => {
+  //   const authToken = localStorage.getItem('authToken');
+  //   if (!authToken) {
+  //     router.push('/'); // Redirect to home if not authenticated
+  //   }
+  // }, [router]);
+
+  const renderBodyContent = () => {
+    switch (selectedPage) {
+      case 'course':
+        return <Courses/>;
+      case 'application':
+        return <Applications />;
+    }
+  };
+    
+  return (
+    <div className="h-[100%] absolute w-[100vw] flex flex-col items-center">
+      <DepartmentNavbar setSelectedPage={setSelectedPage}  selectedPage={selectedPage}/>
+      <div className="w-[100%] h-[100%] flex bg-slate-200 justify-center">
+        {renderBodyContent()}
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ client/app/departmentDashboard/pagesDirectory/courses.js
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Courses(){
+    return (
+        <>
+            <h1 className="capitalize text-xl font-bold mt-3">Courses</h1>
+        </>
+    )
+}
